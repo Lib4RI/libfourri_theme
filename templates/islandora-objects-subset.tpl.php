1bb0601f9d85feff10c1bf1233b1f28c9333170f
Mitch MacKenzie <mitchmac@gmail.com>
2016-03-17 14:56:31 -0300

    Remove non-functional markup for object lists

--- templates/islandora-objects-subset.tpl.php
+++ templates/islandora-objects-subset.tpl.php
@@ -6,24 +6,8 @@
  */
 ?>
 <div class="islandora-objects clearfix">
-  <div class="object-sort-show-wrapper">
-    <div class="displaying-items"><?php isset($islandora_solr_result_count) ? print $islandora_solr_result_count : print "";?></div>
-    <div class="islandora-sort"><?php print $solr_sort?></div>
-    <div class="solr-sort-dummy-right">
-    </div>
-  </div>
-
-  <div class="object-mock-table-header">
-    <div class="select-items">
-      <div class="select-items-input-wrapper"></div>
-    </div>
-    <div class="object-mock-pager">
-      <?php print $pager; ?>
-    </div>
-
-    <div class="object-mock-icons">
-      <?php print $secondary_display_profiles;?>
-    </div>
-  </div>
   <?php print $content; ?>
+  <div class="object-mock-pager">
+    <?php print $pager; ?>
+  </div>
 </div>
